b2edbd2dcf83b24a09367013ae3539f72bf0d5c4
Baibhab-7279 <mrbaibhab7279@gmail.com>
2022-08-04 19:44:03 +0530

    finally

--- pages/views.py
+++ pages/views.py
@@ -35,30 +35,30 @@ def home(request):
 
     c = []
     d = []
-#    for i in range(len(a)-1,-1,-1):
-#        b = a[i]
-#        
-#        c.append(b["image"])
-#        # print(c)
-#        e = b["blogtext"]
-#
-#        f = b["email"]
-#        g = f.split("@")
-#        h = f"{g[0]}.jpg"
-#
-#        c.append(e)
-#        c.append(h)
-#        d.append(c)
-#        # print(d)
-#        c = []
-#        # c.append(b["image"])
-#        # d.append(b["blogtext"])
-#    
-#
-#    ch = check()
-#    # ch["images"] = c
-#    ch["content"] = d
+    for i in range(len(a)-1,-1,-1):
+        b = a[i]
+        
+        c.append(b["image"])
+        # print(c)
+        e = b["blogtext"]
 
+        f = b["email"]
+        g = f.split("@")
+        h = f"{g[0]}.jpg"
+
+        c.append(e)
+        c.append(h)
+        d.append(c)
+        # print(d)
+        c = []
+        # c.append(b["image"])
+        # d.append(b["blogtext"])
+    
+
+    ch = check()
+    # ch["images"] = c
+    ch["content"] = d
+    
     if(ans == True):
         a1 = UserData.objects.filter(choise="private").values("image", "blogtext","email")
         print(a)
